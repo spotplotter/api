d783e17a82d975b98ebce748aae1bacb03368891
Rob Pickerill <r.pickerill@gmail.com>
2025-01-03 15:55:13 +0000

    add origin

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://spotplotter.com",
     ],
     allow_credentials=True,
     allow_methods=["POST"],
