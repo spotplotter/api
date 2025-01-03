3ebeb03fc886bbeb54f23a03161b80d695bf00ee
Rob Pickerill <r.pickerill@gmail.com>
2025-01-03 10:32:02 +0000

    add cors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.routes import router as api_router
 from app.api.v1.limiter import limiter, rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
 app = FastAPI(title="Inference API")
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 # Include API routes
 app.include_router(api_router, prefix="/api/v1")
